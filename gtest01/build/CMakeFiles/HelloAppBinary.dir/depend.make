104ea4b4299caf6dae7b7ae5c6af457e26900ab7
tahirZmn <tahir.zaman@errecompany.org>
2023-06-30 15:25:07 +0200

    resolve the issue of dynamin libraries

--- gtest01/build/CMakeFiles/HelloAppBinary.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for HelloAppBinary.
-# This may be replaced when dependencies are built.
--- gtest01/build/CMakeFiles/operations.dir/src/operations.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/operations.dir/src/operations.cpp.obj: \
- C:\Users\t.zaman\Desktop\workspace\gtest\gtest01\source\src\operations.cpp
